6824ad8c48520a7dea4df2ae66febff05fb71f6f
mbargiel <mbargiel@btopservice.net>
2025-06-24 22:47:16 +0200

    Fix logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,14 @@
-'use client'
-
 import Image from 'next/image'
 import Link from 'next/link'
+
+import logo from '../../public/images/logo.svg'
 
 export default function Navbar({ className = '' }) {
   return (
     <nav className={`w-full z-50 ${className}`}>
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4 text-white">
         <Link href="/" className="flex items-center">
-          <Image
-            src="/images/logo.png"
-            alt="Rezydencja Zawoja Logo"
-            width={150}
-            height={40}
-            priority
-            className="h-auto w-auto"
-          />
+          <Image src={logo} alt="Rezydencja Zawoja Logo" width={150} height={40} priority />
         </Link>
         <ul className="flex gap-6 font-medium">
           <li>
